a915c5c4116784f3e43f8ff6f1d7695d93563d08
EC2 Default User <ec2-user@ip-172-31-58-207.ec2.internal>
2019-11-30 04:15:10 +0000

    lambda v4

--- hello-world/app.js
+++ hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
     response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: "Version-3"
+        message: "Version-4"
         // location: ret.data.trim()
       })
     };
